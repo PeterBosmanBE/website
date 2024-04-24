a0581fbc9edade118896264d1734c183a898b0fa
Peter <148058901+PeterBosmanBE@users.noreply.github.com>
2024-04-24 12:10:42 +0200

    Added typewriter and cool background

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
     return (
-        <main className="flex min-h-screen items-center justify-center p-24">
-            <div className="">
+        <main className="flex items-center justify-center p-24">
+            <div className="text-white">
                 <div className="typewriter">
                     <h1><a href="/">404 - Page not found</a></h1>
                 </div>
