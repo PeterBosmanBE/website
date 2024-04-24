cf829fa86d743bc70b1f48a8b6c7cc233d9d3c2c
Peter <148058901+PeterBosmanBE@users.noreply.github.com>
2024-04-24 12:11:09 +0200

    Added footer to bottom of page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "./components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
-      <title>Portfolio - Peter Bosman</title>
+      <title>Peter Bosman</title>
       <meta name="description" content="Portfolio 'n stuff"/>
       <meta property="og:title" content="Peter Bosman"/>
       <meta property="og:description" content="Portfolio 'n stuff"/>
@@ -22,7 +22,12 @@ export default function RootLayout({
       <meta name="twitter:url" content="https://peterbosman.be"/>
       <meta name="twitter:card" content="summary"/>
     </head>
-    <body className={inter.className}>{children}</body>
+    <body className={inter.className}>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex flex-grow items-center justify-center">{children}</div>
+      <Footer />
+    </div>
+    </body>
     </html>
   );
 }
