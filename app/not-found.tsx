a8caf042943ee3e5d0d4df60c63879a6783b3723
Peter <148058901+PeterBosmanBE@users.noreply.github.com>
2024-04-24 10:57:59 +0200

    Error page added

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+    return (
+        <main className="flex min-h-screen items-center justify-center p-24">
+            <div className="">
+                <div className="typewriter">
+                    <h1><a href="/">404 - Page not found</a></h1>
+                </div>
+            </div>
+        </main>
+    )
+}
