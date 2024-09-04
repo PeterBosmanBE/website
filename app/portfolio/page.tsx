9dd50bd3afdd0f02ad5df54258d0912b80f9f109
Peter <148058901+PeterBosmanBE@users.noreply.github.com>
2024-09-04 16:57:38 +0200

    Coming Soon page for portfolio

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+export default function Portfolio() {
+    return (
+        <main className="flex items-center justify-center">
+            <div className="text-white">
+                <div className="typewriter">
+                    <h1>Coming Soon...</h1>
+                </div>
+            </div>
+        </main>
+    );
+}
