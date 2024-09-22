8264c8028ad06e1819e9f0e6a95dc99fd6227027
Peter <148058901+PeterBosmanBE@users.noreply.github.com>
2024-09-22 21:25:43 +0200

    Added header with my name on it

---
+++ app/components/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../header.css';
+
+export function Header() {
+    return (
+        <div className="header typewriter">
+            <a href="/">
+                <h1>Peter Bosman</h1>
+            </a>
+        </div>
+    )
+}
