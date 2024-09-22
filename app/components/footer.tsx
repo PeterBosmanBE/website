cd944e525c09826b7bef23fd2ef56f8973f8b228
Peter <148058901+PeterBosmanBE@users.noreply.github.com>
2024-09-22 21:30:56 +0200

    Reformated footer code

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,43 +2,41 @@ import React from 'react';
 import Image from "next/image";
 
 export default function Footer() {
-        return (
-            <React.Fragment>
-                <footer>
+    return (
+        <footer>
+            <div>
                 <div>
-                    <div>
-                        <div className="flex items-center justify-center pb-2">
-                            <a className="pr-3" href="https://www.linkedin.com/in/peterbosmanbe">
-                                <Image
-                                    src="/socialmedia/linkedin.png"
-                                    alt="LinkedIn"
-                                    width={40}
-                                    height={40}
-                                    priority
-                                />
-                            </a>
-                            <a className="pr-3" href="https://github.com/PeterBosmanBE">
-                                <Image
-                                    src="/socialmedia/github.png"
-                                    alt="GitHub"
-                                    width={40}
-                                    height={40}
-                                    priority
-                                />
-                            </a>
-                            <a rel="me" href="https://mastodon.social/@peterbosman">
-                                <Image
-                                    src="/socialmedia/mastodon.png"
-                                    alt="Mastodon"
-                                    width={40}
-                                    height={40}
-                                    priority
-                                />
-                            </a>
-                        </div>
+                    <div className="flex items-center justify-center pb-2">
+                        <a className="pr-3" href="https://www.linkedin.com/in/peterbosmanbe">
+                            <Image
+                                src="/socialmedia/linkedin.png"
+                                alt="LinkedIn"
+                                width={40}
+                                height={40}
+                                priority
+                            />
+                        </a>
+                        <a className="pr-3" href="https://github.com/PeterBosmanBE">
+                            <Image
+                                src="/socialmedia/github.png"
+                                alt="GitHub"
+                                width={40}
+                                height={40}
+                                priority
+                            />
+                        </a>
+                        <a rel="me" href="https://mastodon.social/@peterbosman">
+                            <Image
+                                src="/socialmedia/mastodon.png"
+                                alt="Mastodon"
+                                width={40}
+                                height={40}
+                                priority
+                            />
+                        </a>
                     </div>
                 </div>
-                </footer>
-            </React.Fragment>
-        );
+            </div>
+        </footer>
+    );
 }
