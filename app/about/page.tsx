d1a2224eb91b75c896384d9bf94100b9e21f453a
Peter <148058901+PeterBosmanBE@users.noreply.github.com>
2024-09-22 21:23:43 +0200

    Created an about page for myself :P

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import { Header } from '../components/header';
+
+export default function Portfolio() {
+    return (
+        <main className="flex items-center justify-center">
+            <Header />
+            <div className="text-white">
+                <div>
+                    <h1>I am Peter Bosman, a 21 year old programmer from Belgium</h1>
+                </div>
+            </div>
+        </main>
+    );
+}
