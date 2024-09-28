66548dfb9dea224cdb712902e6b5972c2c832246
Peter <148058901+PeterBosmanBE@users.noreply.github.com>
2024-09-28 03:15:09 +0200

    Wrote a small introduction about myself :3

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,8 +7,11 @@ export default function Portfolio() {
             <Header />
             <div className="text-white text-center">
                 <div>
-                    <h1>I am Peter Bosman, a 21 year old programmer from Belgium</h1>
-                    <p>Click <a href="/portfolio">here</a> to go to my <a href="/portfolio">portfolio.</a></p>
+                    <h1 className={"mb-5"}>I am Peter Bosman, a 21 year old programmer from Belgium</h1>
+                    <p>I have a passion for Virtual Reality, Music, Programming, and Virtual Machines.</p>
+                    <p>I am currently learning Javascript, Docker, React, Spring Boot, and more.</p>
+                    <p className={"mt-5"}>Click <a href="/assets/CV.pdf">here</a> to go to see my <a href="/assets/CV.pdf">CV.</a></p>
+                    <p className={"mt-5"}>Click <a href="/portfolio">here</a> to go to my <a href="/portfolio">portfolio.</a></p>
                 </div>
             </div>
         </main>
