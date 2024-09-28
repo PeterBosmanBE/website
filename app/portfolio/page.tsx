3330b3ba9143a2cb87dedb7870464c1cd9270358
Peter <148058901+PeterBosmanBE@users.noreply.github.com>
2024-09-28 03:03:37 +0200

    Added the Cards to the portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,11 +1,17 @@
+"use client";
+
+import { Header } from "../components/header";
+import React from "react";
+import Cards from "./cards";
+
 export default function Portfolio() {
     return (
-        <main className="flex items-center justify-center">
-            <div className="text-white">
-                <div className="typewriter">
-                    <h1>Coming Soon...</h1>
-                </div>
+        <main className="flex items-center justify-center flex-col">
+            <Header />
+            <div className="text-white text-center my-4">
+                <p>Here are some of the current projects I have worked on:</p>
             </div>
+            <Cards />
         </main>
     );
 }
