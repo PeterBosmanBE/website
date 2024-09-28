8ed49a5f5eb5085778c372b6a5685360181cd347
Peter <148058901+PeterBosmanBE@users.noreply.github.com>
2024-09-28 03:06:27 +0200

    Centered the text and added a way to my portfolio

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,14 @@
 import { Header } from '../components/header';
+import React from "react";
 
 export default function Portfolio() {
     return (
         <main className="flex items-center justify-center">
             <Header />
-            <div className="text-white">
+            <div className="text-white text-center">
                 <div>
                     <h1>I am Peter Bosman, a 21 year old programmer from Belgium</h1>
+                    <p>Click <a href="/portfolio">here</a> to go to my <a href="/portfolio">portfolio.</a></p>
                 </div>
             </div>
         </main>
