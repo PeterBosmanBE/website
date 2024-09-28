1e29a867d6041aa3e30df211b0cff57d238672c3
Peter <148058901+PeterBosmanBE@users.noreply.github.com>
2024-09-28 03:02:32 +0200

    Portfolio Data added

---
+++ app/data/data.ts
@@ -0,0 +1,20 @@
+const projects = [
+    {
+        id: 1,
+        title: "VRHeaven",
+        description: "This is my first school project made with basic HTML, CSS and Bootstrap",
+        dateOfProject: "16 January 2024",
+        image: "/assets/images/VRHeaven.png",
+        link: "https://themasite.peterbosman.be/index.html"
+    },
+    {
+        id: 2,
+        title: "DishCover",
+        description: "DishCover is a team project made by Peter, Carolina, Rob and Mohamed.",
+        dateOfProject: "22 April 2024 - 9 June 2024",
+        image: "/assets/images/DishCover.png",
+        link: "https://dishcover-frkr2pe4bq-ew.a.run.app/recipeslist"
+    },
+];
+
+export default projects;
