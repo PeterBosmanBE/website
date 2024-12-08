0aef490c517c8e145dae13c689b85f70ccceafbc
Peter Bosman <info@peterbosman.be>
2024-12-08 22:40:55 +0100

    Changed my age from 21 to 22 since it was my birthday :3

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ export default function Portfolio() {
             <Header />
             <div className="text-white text-center">
                 <div>
-                    <h1 className={"mb-5"}>I am Peter Bosman, a 21 year old programmer from Belgium</h1>
+                    <h1 className={"mb-5"}>I am Peter Bosman, a 22 year old programmer from Belgium</h1>
                     <p>I have a passion for Virtual Reality, Music, Programming, and Virtual Machines.</p>
                     <p>I am currently learning Javascript, Docker, React, Spring Boot, and more.</p>
                     <p className={"mt-5"}>Click <a href="/assets/CV.pdf">here</a> to go to see my <a href="/assets/CV.pdf">CV.</a></p>
