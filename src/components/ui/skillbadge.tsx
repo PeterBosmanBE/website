24ae1b13cc9f0c6a52185c34f49aab368940389a
Peter Bosman <info@peterbosman.be>
2025-12-11 07:15:24 +0100

    Added customName for easier name changes depending on what project the badge is being added to

--- src/components/ui/skillbadge.tsx
+++ src/components/ui/skillbadge.tsx
@@ -4,17 +4,20 @@ type SkillName = keyof typeof Skills;
 
 type SkillType = {
   skillName: SkillName;
+  customName?: string;
 };
 
-export default function SkillBadge({ skillName }: SkillType) {
+export default function SkillBadge({ skillName, customName }: SkillType) {
   const skill = Skills[skillName];
+
+  const skillTitle = customName ? customName : skill.title;
 
   return (
     <div className="flex items-center gap-2">
       <a href={skill?.link}>
         <div className="flex items-center outline rounded-md p-2 pr-2 gap-1 gap-2 transform">
           <div className="w-6 h-6 flex-shrink-0">{skill?.icon()}</div>
-          <div className="text-[20px] leading-[25px]">{skill?.title}</div>
+          <div className="text-[20px] leading-[25px]">{skillTitle}</div>
         </div>
       </a>
     </div>
