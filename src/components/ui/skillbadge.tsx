b10be1a60a3cd076c5e5700b119f6d6dbfdfcd4e
Peter Bosman <info@peterbosman.be>
2025-12-11 07:18:26 +0100

    Added custom URL's for easy URL changes depending from project to project

--- src/components/ui/skillbadge.tsx
+++ src/components/ui/skillbadge.tsx
@@ -5,16 +5,18 @@ type SkillName = keyof typeof Skills;
 type SkillType = {
   skillName: SkillName;
   customName?: string;
+  customURL?: string;
 };
 
-export default function SkillBadge({ skillName, customName }: SkillType) {
+export default function SkillBadge({ skillName, customName, customURL }: SkillType) {
   const skill = Skills[skillName];
 
   const skillTitle = customName ? customName : skill.title;
+  const skillURL = customURL ? customURL : skill.link;
 
   return (
     <div className="flex items-center gap-2">
-      <a href={skill?.link}>
+      <a href={skillURL}>
         <div className="flex items-center outline rounded-md p-2 pr-2 gap-1 gap-2 transform">
           <div className="w-6 h-6 flex-shrink-0">{skill?.icon()}</div>
           <div className="text-[20px] leading-[25px]">{skillTitle}</div>
