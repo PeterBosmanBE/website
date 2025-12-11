1cb9c5926718804a0aa71131970561677a862964
Peter Bosman <info@peterbosman.be>
2025-12-11 07:32:45 +0100

    Changed portfolio project cards from Ant Design to ShadCN

--- src/app/portfolio/cards.tsx
+++ src/app/portfolio/cards.tsx
@@ -1,33 +1,27 @@
 "use client";
 
-import { Card } from "antd";
-import React from "react";
+import { Card } from "../../components/ui/cards";
 import projects from "../../data/data";
 
-const { Meta } = Card;
-
-const Cards = () => {
-    return (
-        <div className="text-white flex flex-wrap justify-center">
-            {projects.map((project) => (
-                <div key={project.id} className="m-4">
-                    <a href={project.link} target="_blank" rel="noopener noreferrer">
-                        <Card
-                            hoverable
-                            style={{ width: 240 }}
-                            cover={<img alt={project.title} src={project.image} />}
-                        >
-                            <Meta title={project.title} description={project.description} /><br />
-                            <div>
-                                <div>Date of project:</div>
-                                <div>{project.dateOfProject}</div>
-                            </div>
-                        </Card>
-                    </a>
-                </div>
-            ))}
+export default function Cards() {
+  return (
+    <div className="text-white flex flex-wrap justify-center">
+      {projects.map((project) => (
+        <div key={project.id} className="m-4">
+          <a href={project.link} target="_blank" rel="noopener noreferrer">
+            <Card style={{ width: 240 }}>
+              <img src={project.image} alt={project.title} />
+              <div>
+                <div>{project.title}</div> <div>{project.description}</div>
+              </div>
+              <div>
+                <div>Date of project:</div>
+                <div>{project.dateOfProject}</div>
+              </div>
+            </Card>
+          </a>
         </div>
-    );
-};
-
-export default Cards;
+      ))}
+    </div>
+  );
+}
