a55c824fb16eeb9c45edd6558ebb6db8216c1eef
Peter Bosman <info@peterbosman.be>
2025-12-11 07:09:54 +0100

    Added icons and skills

---
+++ src/data/skills.ts
@@ -0,0 +1,125 @@
+import { Icons } from "../components/ui/icons";
+
+export const Skills = {
+  // Languages
+  TypeScript: {
+    title: "TypeScript",
+    icon: Icons.TypeScript,
+    link: "https://www.typescriptlang.org/",
+  },
+  JavaScript: {
+    title: "JavaScript",
+    icon: Icons.JavaScript,
+    link: "https://www.javascript.com/",
+  },
+  HTML: {
+    title: "HTML",
+    icon: Icons.HTML,
+    link: "https://html.com/",
+  },
+  CSS: {
+    title: "CSS",
+    icon: Icons.CSS,
+    link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  TailwindCSS: {
+    title: "Tailwind CSS",
+    icon: Icons.TailwindCSS,
+    link: "https://tailwindcss.com/",
+  },
+  Bootstrap: {
+    title: "Bootstrap",
+    icon: Icons.Bootstrap,
+    link: "https://getbootstrap.com/",
+  },
+  
+  // Frameworks
+  NextJS: {
+    title: "Next.js",
+    icon: Icons.NextJS,
+    link: "",
+  },
+  
+  // Back-end
+  Hono: {
+    title: "Hono",
+    icon: Icons.Hono,
+    link: "https://hono.dev/",
+  },
+  SpringBoot: {
+    title: "Spring Boot",
+    icon: Icons.SpringBoot,
+    link: "https://spring.io/projects/spring-boot",
+  },
+  
+  // SQL (Databases)
+  PostgreSQL: {
+    title: "PostgreSQL",
+    icon: Icons.PostgreSQL,
+    link: "https://www.postgresql.org/",
+  },
+  MongoDB: {
+    title: "MongoDB",
+    icon: Icons.MongoDB,
+    link: "https://www.mongodb.com/",
+  },
+  MySQL: {
+    title: "MySQL",
+    icon: Icons.MySQL,
+    link: "https://www.mysql.com/",
+  },
+  H2: {
+    title: "H2-Database",
+    icon: Icons.H2,
+    link: "https://www.h2database.com/",
+  },
+  Firebase: {
+    title: "Firebase",
+    icon: Icons.Firebase,
+    link: "https://firebase.google.com/",
+  },
+  
+  // Cloud Services
+  GoogleCloud: {
+    title: "Google Cloud",
+    icon: Icons.GoogleCloud,
+    link: "https://cloud.google.com/",
+  },
+  Cloudflare: {
+    title: "Cloudflare",
+    icon: Icons.Cloudflare,
+    link: "https://www.cloudflare.com/",
+  },
+  Vercel: {
+    title: "Vercel",
+    icon: Icons.Vercel,
+    link: "https://www.vercel.com",
+  },
+  
+  // Other
+  WindowsServer: {
+    title: "Windows Server",
+    icon: Icons.WindowsServer,
+    link: "https://www.microsoft.com/en-us/windows-server",
+  },
+  Docker: {
+    title: "Docker",
+    icon: Icons.Docker,
+    link: "https://www.docker.com/",
+  },
+  S3: {
+    title: "S3",
+    icon: Icons.S3,
+    link: "https://www.min.io/",
+  },
+  Git: {
+    title: "Git",
+    icon: Icons.Git,
+    link: "https://git-scm.com/",
+  },
+  GitHub: {
+    title: "GitHub",
+    icon: Icons.GitHub,
+    link: "https://github.com/",
+  }
+};
